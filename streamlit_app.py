cb26f46e6b6c62c8ab8ad89ced23335837edef51
kjsaxton <kjsaxton@syr.edu>
2026-08-31 03:25:54 +0000

    update for pdf reader

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ except:
 if check_key:
     # Let the user upload a file via `st.file_uploader`.
     uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
+        "Upload a document (.txt or .pdf)", type=("txt", "md")
     )
 
     # Ask the user for a question via `st.text_area`.
