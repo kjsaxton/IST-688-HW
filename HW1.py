574823ba810e52416ad4bbf93c5a4d1689bab4d3
kjsaxton <kjsaxton@syr.edu>
2026-08-31 04:23:19 +0000

    gpt change

--- HW1.py
+++ HW1.py
@@ -65,7 +65,7 @@ else:
                 ]
 
                 stream = client.chat.completions.create(
-                    model="gpt-3.5",
+                    model="gpt-4.1",
                     messages=messages,
                     stream=True,
                 )
