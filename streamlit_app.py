82fbfac55b776dbd5c18c0cc7a3b7e340b7c6224
kjsaxton <kjsaxton@syr.edu>
2026-08-31 03:27:06 +0000

    fixed indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,6 @@ openai_api_key = st.text_input("OpenAI API Key", type="password")
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
-
     # Create an OpenAI client.
     client = OpenAI(api_key=openai_api_key)
 
