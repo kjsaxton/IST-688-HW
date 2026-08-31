a30fcdf2b1c96a20da9f55b8cfa2ead441acb558
kjsaxton <kjsaxton@syr.edu>
2026-08-31 02:02:17 +0000

    Added PDfF reader

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 from openai import OpenAI
 
+from pypdf import PdfReader
+
+def read_pdf(uploaded_file)
+    reader=PdfReader(uploaded_file)
+    text =""
+    for page in reader.pages
+        page_text = page.extract_text()
+        if page_text:
+            text += page_text + "/n"
+        return text
+         
 # Show title and description.
 st.title("MY Document question answering")
 st.write(
