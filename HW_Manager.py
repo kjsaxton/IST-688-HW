12c5290fcbee4771bbd97b0eea26960997b8bf33
kjsaxton <kjsaxton@syr.edu>
2026-09-13 14:26:09 -0400

    Add HW3 page to navigation

--- HW_Manager.py
+++ HW_Manager.py
@@ -4,6 +4,7 @@ st.set_page_config(page_title="HW Manager")
 
 HW1 = st.Page('HW/HW1.py', title='HW1')
 HW2 = st.Page('HW/HW2.py', title='HW2', default=True)
+HW3 = st.Page('HW/HW3.py', title='HW3')
 
-pg = st.navigation([HW1, HW2])
+pg = st.navigation([HW1, HW2, HW3])
 pg.run()
