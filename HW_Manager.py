2cb63408bbfb9909731224d40f961f51a056262a
kjsaxton <kjsaxton@syr.edu>
2026-09-14 03:18:03 +0000

    changed navigation

--- HW_Manager.py
+++ HW_Manager.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.set_page_config(page_title="HW Manager")
 
 HW1 = st.Page('HW/HW1.py', title='HW1')
-HW2 = st.Page('HW/HW2.py', title='HW2', default=True)
-HW3 = st.Page('HW/HW3.py', title='HW3')
+HW2 = st.Page('HW/HW2.py', title='HW2')
+HW3 = st.Page('HW/HW3.py', title='HW3', default=True)
 
 pg = st.navigation([HW1, HW2, HW3])
 pg.run()
