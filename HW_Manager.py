7c13ba3cb2f2fe0fa3d71e7efed981466821e2b9
kjsaxton <kjsaxton@syr.edu>
2026-09-18 18:40:35 +0000

    added hw4 to navigation and made it the default

--- HW_Manager.py
+++ HW_Manager.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="HW Manager")
 
 HW1 = st.Page('HW/HW1.py', title='HW1')
 HW2 = st.Page('HW/HW2.py', title='HW2')
-HW3 = st.Page('HW/HW3.py', title='HW3', default=True)
-
-pg = st.navigation([HW1, HW2, HW3])
+HW3 = st.Page('HW/HW3.py', title='HW3')
+HW4 = st.Page('HW/HW4.py', title='HW4', default=True)
+pg = st.navigation([HW1, HW2, HW3, HW4])
 pg.run()
