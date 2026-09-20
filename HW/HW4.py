1061bf7f3a4f9ffef28655414e8abb07bba67d50
kjsaxton <kjsaxton@syr.edu>
2026-09-20 22:27:03 +0000

    changed directory

--- HW/HW4.py
+++ HW/HW4.py
@@ -99,7 +99,7 @@ def load_html_to_collection(folder_path, collection):
  
 # Check if collection is empty and load PDFs
 if collection.count() == 0:
-   loaded = load_html_to_collection('.HW/HW-04-Data/', collection)
+   loaded = load_html_to_collection('HW/HW-04-Data/', collection)
    st.sidebar.write(f"Loaded {len(loaded)} files, collection now has {collection.count()} chunks")
 
 if 'HW4_VectorDB' not in st.session_state:
