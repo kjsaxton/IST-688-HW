1e6f06b0fae3920b014346031ef70ae47067ee2c
kjsaxton <kjsaxton@syr.edu>
2026-09-21 01:51:50 +0000

    removed initial files

--- HW/HW4.py
+++ HW/HW4.py
@@ -99,7 +99,7 @@ def load_html_to_collection(folder_path, collection):
  
 # Check if collection is empty and load PDFs
 if collection.count() == 0:
-   loaded = load_html_to_collection('HW/HW-04-Data/', collection)
+   loaded = load_html_to_collection('Su_orgs/', collection)
 
 st.sidebar.write(f"Collection currently has {collection.count()} chunks")
 
